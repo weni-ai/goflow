fedf03a6edc3eae3dfded2e6b427b296fc232073
Rafael Soares <rafael.dvlpr@gmail.com>
2023-10-10 19:00:22 -0300

    msg catalog, add product search att

--- flows/msg_catalog.go
+++ flows/msg_catalog.go
@@ -10,30 +10,32 @@ import (
 type MsgCatalog struct {
 	BaseMsg
 
-	Header_      string        `json:"header,omitempty"`
-	Body_        string        `json:"body,omitempty"`
-	Footer_      string        `json:"footer,omitempty"`
-	Products_    []string      `json:"products,omitempty"`
-	Action_      string        `json:"action,omitempty"`
-	Topic_       MsgTopic      `json:"topic,omitempty"`
-	Smart_       bool          `json:"smart"`
-	TextLanguage envs.Language `json:"text_language,omitempty"`
+	Header_        string        `json:"header,omitempty"`
+	Body_          string        `json:"body,omitempty"`
+	Footer_        string        `json:"footer,omitempty"`
+	Products_      []string      `json:"products,omitempty"`
+	Action_        string        `json:"action,omitempty"`
+	Topic_         MsgTopic      `json:"topic,omitempty"`
+	Smart_         bool          `json:"smart"`
+	ProductSearch_ string        `json:"product_search,omitempty"`
+	TextLanguage   envs.Language `json:"text_language,omitempty"`
 }
 
-func NewMsgCatalog(urn urns.URN, channel *assets.ChannelReference, header string, body string, footer string, products []string, action string, smart bool, topic MsgTopic) *MsgCatalog {
+func NewMsgCatalog(urn urns.URN, channel *assets.ChannelReference, header, body, footer, action, productSearch string, products []string, smart bool, topic MsgTopic) *MsgCatalog {
 	return &MsgCatalog{
 		BaseMsg: BaseMsg{
 			UUID_:    MsgUUID(uuids.New()),
 			URN_:     urn,
 			Channel_: channel,
 		},
-		Header_:   header,
-		Body_:     body,
-		Footer_:   footer,
-		Products_: products,
-		Action_:   action,
-		Smart_:    smart,
-		Topic_:    topic,
+		Header_:        header,
+		Body_:          body,
+		Footer_:        footer,
+		Products_:      products,
+		Action_:        action,
+		Smart_:         smart,
+		ProductSearch_: productSearch,
+		Topic_:         topic,
 	}
 }
 
@@ -50,3 +52,5 @@ func (m *MsgCatalog) Topic() MsgTopic { return m.Topic_ }
 func (m *MsgCatalog) Action() string { return m.Action_ }
 
 func (m *MsgCatalog) Smart() bool { return m.Smart_ }
+
+func (m *MsgCatalog) ProductSearch() string { return m.ProductSearch_ }
