1723c5b9d1f6505c15f5dc28a5a37d3630f6fe40
Robi9 <robertasousa714@gmail.com>
2023-11-01 18:46:59 -0300

    Add test for msgCatalogOut

--- flows/msg_catalog_test.go
+++ flows/msg_catalog_test.go
@@ -1,83 +1,57 @@
 package flows_test
 
-// func TestCatalogMsg(t *testing.T) {
-// 	uuids.SetGenerator(uuids.NewSeededGenerator(12345))
-// 	defer uuids.SetGenerator(uuids.DefaultGenerator)
+import (
+	"testing"
 
-// 	msg := flows.NewMsgCatalog(
-// 		urns.URN("whatsapp:+558299988776655"),
-// 		assets.NewChannelReference(assets.ChannelUUID("1fbd661a-d519-42c5-9bbe-d12a6da9621b"), "My WPP Cloud"),
-// 		"Header text",
-// 		"Body text",
-// 		"Footer text",
-// 		"View Products",
-// 		"",
-// 		[]string{
-// 			"524580ca-406d-491b-b97e-b07113e322db",
-// 			"ee48c9ed-6e52-4a76-8e7b-70c5046c559a",
-// 		},
-// 		false,
-// 		flows.MsgTopic("none"),
-// 	)
+	"github.com/nyaruka/gocommon/jsonx"
+	"github.com/nyaruka/gocommon/urns"
+	"github.com/nyaruka/gocommon/uuids"
+	"github.com/nyaruka/goflow/assets"
+	"github.com/nyaruka/goflow/flows"
+	"github.com/nyaruka/goflow/test"
+	"github.com/stretchr/testify/require"
+)
 
-// 	marshaled, err := jsonx.Marshal(msg)
-// 	require.NoError(t, err)
+func TestMsgCatalogOut(t *testing.T) {
 
-// 	test.AssertEqualJSON(t, []byte(`{
-// 		"action": "View Products",
-// 		"body": "Body text",
-// 		"channel": {
-// 				"name": "My WPP Cloud",
-// 				"uuid": "1fbd661a-d519-42c5-9bbe-d12a6da9621b"
-// 		},
-// 		"footer": "Footer text",
-// 		"header": "Header text",
-// 		"products": [
-// 				"524580ca-406d-491b-b97e-b07113e322db",
-// 				"ee48c9ed-6e52-4a76-8e7b-70c5046c559a"
-// 		],
-// 		"smart": false,
-// 		"text": "",
-// 		"topic": "none",
-// 		"urn": "whatsapp:+558299988776655",
-// 		"uuid": "1ae96956-4b34-433e-8d1a-f05fe6923d6d"
-// 	}`), marshaled)
-// }
+	uuids.SetGenerator(uuids.NewSeededGenerator(12345))
+	defer uuids.SetGenerator(uuids.DefaultGenerator)
 
-// func TestCatalogMsgSmart(t *testing.T) {
-// 	uuids.SetGenerator(uuids.NewSeededGenerator(12345))
-// 	defer uuids.SetGenerator(uuids.DefaultGenerator)
+	msg := flows.NewMsgCatalogOut(
+		urns.URN("tel:+1234567890"),
+		assets.NewChannelReference(assets.ChannelUUID("61f38f46-a856-4f90-899e-905691784159"), "My Android"),
+		"header",
+		"body",
+		"footer",
+		"action",
+		"productSearch",
+		[]string{"product_1", "product_2"},
+		false,
+		flows.MsgTopic("none"),
+		false,
+	)
 
-// 	msg := flows.NewMsgCatalog(
-// 		urns.URN("whatsapp:+558299988776655"),
-// 		assets.NewChannelReference(assets.ChannelUUID("1fbd661a-d519-42c5-9bbe-d12a6da9621b"), "My WPP Cloud"),
-// 		"Header text",
-// 		"Body text",
-// 		"Footer text",
-// 		"View Products",
-// 		"product search text",
-// 		[]string{},
-// 		true,
-// 		flows.MsgTopic("none"),
-// 	)
+	marshaled, err := jsonx.Marshal(msg)
+	require.NoError(t, err)
 
-// 	marshaled, err := jsonx.Marshal(msg)
-// 	require.NoError(t, err)
-
-// 	test.AssertEqualJSON(t, []byte(`{
-// 		"action": "View Products",
-// 		"body": "Body text",
-// 		"channel": {
-// 				"name": "My WPP Cloud",
-// 				"uuid": "1fbd661a-d519-42c5-9bbe-d12a6da9621b"
-// 		},
-// 		"footer": "Footer text",
-// 		"header": "Header text",
-// 		"product_search": "product search text",
-// 		"smart": true,
-// 		"text": "",
-// 		"topic": "none",
-// 		"urn": "whatsapp:+558299988776655",
-// 		"uuid": "1ae96956-4b34-433e-8d1a-f05fe6923d6d"
-// 	}`), marshaled)
-// }
+	test.AssertEqualJSON(t, []byte(`{
+		"action": "action",
+		"body": "body",
+		"channel": {
+			"name": "My Android",
+			"uuid": "61f38f46-a856-4f90-899e-905691784159"
+		},
+		"footer": "footer",
+		"header": "header",
+		"product_search": "productSearch",
+		"products": [
+			"product_1",
+			"product_2"
+		],
+		"smart": false,
+		"text": "",
+		"topic": "none",
+		"urn": "tel:+1234567890",
+		"uuid": "1ae96956-4b34-433e-8d1a-f05fe6923d6d"
+	}`), marshaled, "JSON mismatch")
+}
