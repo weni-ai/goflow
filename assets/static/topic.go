29bd5b5e9ca56d5cc32a385ffa0967752d611383
Rafael Soares <rafael.dvlpr@gmail.com>
2024-10-28 17:10:00 -0300

    add to topic queue_uuid attribute

--- assets/static/topic.go
+++ assets/static/topic.go
@@ -6,15 +6,17 @@ import (
 
 // Topic is a JSON serializable implementation of a topic asset
 type Topic struct {
-	UUID_ assets.TopicUUID `json:"uuid" validate:"required,uuid"`
-	Name_ string           `json:"name"`
+	UUID_      assets.TopicUUID `json:"uuid" validate:"required,uuid"`
+	Name_      string           `json:"name"`
+	QueueUUID_ assets.QueueUUID `json:"queue_uuid"`
 }
 
 // NewTopic creates a new topic
-func NewTopic(uuid assets.TopicUUID, name string) assets.Topic {
+func NewTopic(uuid assets.TopicUUID, name string, queueUUID assets.QueueUUID) assets.Topic {
 	return &Topic{
-		UUID_: uuid,
-		Name_: name,
+		UUID_:      uuid,
+		Name_:      name,
+		QueueUUID_: queueUUID,
 	}
 }
 
@@ -23,3 +25,6 @@ func (t *Topic) UUID() assets.TopicUUID { return t.UUID_ }
 
 // Name returns the name of this ticketer
 func (t *Topic) Name() string { return t.Name_ }
+
+// Queue returns the queue UUID of this ticketer->wenichats->queue UUID
+func (t *Topic) QueueUUID() assets.QueueUUID { return t.QueueUUID_ }
