ab8e3060fdfc567ecd92a56899a1b7d57cf3b9f8
lucaslinhares <lucaslinhares027@gmail.com>
2026-08-11 21:17:03 -0300

    Update WENI-CHANGELOG

--- contactql/evaluator_test.go
+++ contactql/evaluator_test.go
@@ -26,8 +26,10 @@ func TestEvaluateQuery(t *testing.T) {
 		"name":     []interface{}{"Bob Smithwick"},
 		"tel":      []interface{}{"+59313145145"},
 		"twitter":  []interface{}{"bob_smith"},
-		"whatsapp": []interface{}{},
-		"gender":   []interface{}{"male"},
+		"whatsapp":        []interface{}{},
+		"whatsapp_bsuid":  []interface{}{"BR.1583576196772655"},
+		"whatsapp_phone":  []interface{}{},
+		"gender":          []interface{}{"male"},
 		"age":      []interface{}{decimal.NewFromFloat(36)},
 		"dob":      []interface{}{time.Date(1981, 5, 28, 13, 30, 23, 0, time.UTC)},
 		"state":    []interface{}{"Kigali"},
@@ -71,6 +73,12 @@ func TestEvaluateQuery(t *testing.T) {
 		{query: `twitter:jim_smith`, result: false},
 		{query: `twitter ~ smith`, result: true},
 		{query: `whatsapp = 4533343`, result: false},
+		{query: `whatsapp_bsuid != ""`, result: true},
+		{query: `whatsapp_bsuid = ""`, result: false},
+		{query: `whatsapp_phone != ""`, result: false},
+		{query: `whatsapp_phone = ""`, result: true},
+		{query: `whatsapp_bsuid != "" AND whatsapp_phone = "" AND tel = ""`, result: false}, // tel is set on testObj
+		{query: `whatsapp_bsuid != "" AND whatsapp_phone = ""`, result: true},
 
 		// text field condition
 		{query: `Gender = male`, result: true},
--- contactql/parser.go
+++ contactql/parser.go
@@ -161,6 +161,13 @@ func (c *Condition) validate(env envs.Environment, resolver Resolver) error {
 		}
 	}
 
+	// whatsapp_bsuid / whatsapp_phone only support existence checks (is set / is not set)
+	if c.propKey == AttributeWhatsAppBSUID || c.propKey == AttributeWhatsAppPhone {
+		if !(c.operator == OpEqual || c.operator == OpNotEqual) || c.value != "" {
+			return NewQueryError(ErrUnsupportedComparison, "%s can only be checked as set or not set", c.propKey).withExtra("property", c.propKey).withExtra("operator", string(c.operator))
+		}
+	}
+
 	// if existence check, disallow certain attributes
 	if (c.operator == OpEqual || c.operator == OpNotEqual) && c.value == "" {
 		switch c.propKey {
